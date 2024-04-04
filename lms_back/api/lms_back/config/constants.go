cfd06ca0f22ca6f7c315adaaa574df0620bb0b63
Akromjon Atabayev <146443995+Akrom0181@users.noreply.github.com>
2024-04-04 17:46:55 +0500

    service

---
+++ lms_back/api/lms_back/config/constants.go
@@ -0,0 +1,13 @@
+package config
+
+import "time"
+
+const (
+	ERR_INFORMATION     = "The server has received the request and is continuing the process"
+	SUCCESS             = "The request was successful"
+	ERR_REDIRECTION     = "You have been redirected and the completion of the request requires further action"
+	ERR_BADREQUEST      = "Bad request"
+	ERR_INTERNAL_SERVER = "While the request appears to be valid, the server could not complete the request"
+)
+
+const  Timeout = time.Second * 2
